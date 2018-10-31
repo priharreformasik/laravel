42d4f1a54312ab56ab6a11ddc249625bd0cbe44e
Prihar Reformasi Kusumaningrum <refoningrum12@gmail.com>
2018-10-31 21:49:13 +0700

    add templates

--- routes/web.php
+++ routes/web.php
@@ -23,3 +23,12 @@ Route::get('pesan', function () {
 Route::post('pesan/kirim', function () {
     return view('pesan-kirim');
 });
+Route::get('templates', function () {
+    return view('layouts.master');
+});
+Route::get('coba1', function () {
+    return view('coba1');
+});
+Route::get('coba2', function () {
+    return view('coba2');
+});
